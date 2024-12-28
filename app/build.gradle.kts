5bd3644560240de7ee0b0714170e6e96cce7a378
Haziel Covarrubias <67761409+HazielCC@users.noreply.github.com>
2024-12-28 13:25:39 -0700

    Actualizar versiones de Kotlin y dependencias, agregar soporte para KSP y mejorar comentarios en build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
+    // Alias para el plugin de aplicación Android.
     alias(libs.plugins.android.application)
+    // Alias para el plugin de Kotlin para Android.
     alias(libs.plugins.kotlin.android)
+    // Alias para el plugin KSP (Kotlin Symbol Processing).
+    alias(libs.plugins.ksp)
+    // Alias para el plugin de Hilt para la inyección de dependencias en Android.
+    alias(libs.plugins.hilt.android)
+    // Plugin para procesamiento de anotaciones en Kotlin (kapt).
     id("kotlin-kapt") // Kotlin annotation processing
-    id("com.google.dagger.hilt.android") // DaggerHilt
 }
 
 android {
@@ -42,35 +48,38 @@ android {
 }
 
 dependencies {
-    // Retrofit
-    implementation(libs.retrofit) // Retrofit
-    implementation(libs.converter.gson) // Gson Converter
-    implementation(libs.logging.interceptor) // Interceptor
-    //Corrutinas
-    implementation(libs.kotlinx.coroutines.android) // Corrutinas
+    // Dependencias para Retrofit
+    implementation(libs.retrofit) // Biblioteca principal de Retrofit para llamadas de red.
+    implementation(libs.converter.gson) // Convertidor Gson para Retrofit.
+    implementation(libs.logging.interceptor) // Interceptor de logging para Retrofit.
 
-    // Life Cycle Components
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.livedata.ktx) // LiveData
+    // Dependencias para Corrutinas
+    implementation(libs.kotlinx.coroutines.android) // Biblioteca de corutinas para Android.
 
-    // DaggerHilt
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
+    // Componentes del ciclo de vida de Android
+    implementation(libs.androidx.lifecycle.viewmodel.ktx) // ViewModel KTX para ciclo de vida.
+    implementation(libs.androidx.lifecycle.livedata.ktx) // LiveData KTX para ciclo de vida.
 
-    // Room
-    implementation(libs.androidx.room.ktx)
-    kapt(libs.androidx.room.compiler)
+    // Dependencias para Dagger Hilt
+    implementation(libs.hilt.android) // Biblioteca principal de Hilt.
+    ksp(libs.hilt.android.compiler) // Compilador para Hilt.
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    // Dependencias para Room
+    implementation(libs.androidx.room.ktx) // Biblioteca KTX para Room.
+    ksp(libs.androidx.room.compiler) // Compilador para Room.
 
-    testImplementation(libs.junit)
-    testImplementation(libs.mockk)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.androidx.core.testing)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    // Dependencias adicionales
+    implementation(libs.androidx.core.ktx) // Biblioteca core KTX para Android.
+    implementation(libs.androidx.appcompat) // Biblioteca AppCompat para compatibilidad hacia atrás.
+    implementation(libs.material) // Componentes de Material Design.
+    implementation(libs.androidx.activity) // Biblioteca de Activity KTX para Android.
+    implementation(libs.androidx.constraintlayout) // Biblioteca para ConstraintLayout.
+
+    // Dependencias para pruebas
+    testImplementation(libs.junit) // JUnit para pruebas unitarias.
+    testImplementation(libs.mockk) // MockK para mocks en Kotlin.
+    testImplementation(libs.androidx.core.testing) // Biblioteca de pruebas core para Android.
+    androidTestImplementation(libs.androidx.junit) // JUnit para pruebas de instrumentación.
+    androidTestImplementation(libs.androidx.espresso.core) // Espresso para pruebas de UI.
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,30 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Archivo de compilación a nivel superior donde puedes añadir opciones de configuración comunes
+// a todos los subproyectos/módulos.
 plugins {
+    // Alias para el plugin de aplicación Android, no se aplica automáticamente.
     alias(libs.plugins.android.application) apply false
+    // Alias para el plugin de Kotlin para Android, no se aplica automáticamente.
     alias(libs.plugins.kotlin.android) apply false
+    // Alias para el plugin de Hilt (inyección de dependencias), no se aplica automáticamente.
     alias(libs.plugins.hilt.android) apply false
+    // Alias para el plugin KSP (Kotlin Symbol Processing), no se aplica automáticamente.
+    alias(libs.plugins.ksp) apply false
 }
+
+/*
+* Android Application Plugin (com.android.application):
+* Proporciona tareas y configuraciones específicas para aplicaciones Android,
+* como la compilación de código fuente, empaquetado de aplicaciones APK, y gestión de recursos y dependencias.
+
+* Kotlin Android Plugin (org.jetbrains.kotlin.android):
+* Permite compilar código Kotlin, así como integrar características de Kotlin como extensiones y coroutines
+*
+* Hilt Android Plugin (com.google.dagger.hilt.android):
+* Este plugin facilita la configuración de Hilt en proyectos Android.
+* Simplifica la inyección de dependencias proporcionando anotaciones, mejorando la modularidad y testabilidad del código.
+
+* Kotlin Symbol Processing Plugin (com.google.devtools.ksp):
+* KSP (Kotlin Symbol Processing). Facilita la generación de código basado en anotaciones en tiempo de compilación.
+* Es una alternativa a kapt (Kotlin Annotation Processing Tool) y está optimizado para funcionar con Kotlin.
+* */
